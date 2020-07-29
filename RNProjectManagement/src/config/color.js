87c4e47b267e5b400f36e209e198f89187809985
ANNX <xuanan290192@gmail.com>
2020-07-29 21:55:59 +0700

    Add color config

---
+++ RNProjectManagement/src/config/color.js
@@ -0,0 +1,13 @@
+const Color = {
+  red: "red",
+  green: "green",
+  blue: "blue",
+  orange: "orange",
+  tomato: "tomato",
+  white: "#F1F5FE",
+  transparent: "transparent",
+};
+
+export {
+  Color
+}
---
+++ RNProjectManagement/src/config/index.js
@@ -0,0 +1,4 @@
+import { Color } from './color';
+export {
+  Color
+}
